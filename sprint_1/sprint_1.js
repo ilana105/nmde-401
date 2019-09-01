40e5abe01ac0bfaca120cc5e0daaaaafa0caa9d7
Ilana Leva <35470918+ilana105@users.noreply.github.com>
2019-09-01 18:35:58 -0400

    Add files via upload

--- sprint_1/sprint_1.js
+++ sprint_1/sprint_1.js
@@ -3,18 +3,18 @@
  * Sprint #1
  */
 
- const svg = SVG('layers').size(1000, 600);
+ const svg = SVG('drawing').size(1000, 600);
  const shapes = [];
  let index = 0;
  let defaultSize = 100;
- let img1Path = 'img/test1.jpg';
- let img2Path = 'img/test2.jpg';
+ let topImgPath = 'img/top_img.png';
+ let botImgPath = 'img/bottom_img.png';
  let isMouseDown = false;
 
 SVG.on(document, 'DOMContentLoaded', function() {
     if (SVG.supported) {
-        var img2 = svg.image(img2Path).loaded(function(loader) {
-            this.size(loader.width * 0.4, loader.height * 0.4)
+        var img2 = svg.image(topImgPath).loaded(function(loader) {
+            this.size(loader.width * (1/3), loader.height * 0.32)
           })
         img2.move(0,0)
 
@@ -24,31 +24,30 @@ SVG.on(document, 'DOMContentLoaded', function() {
             var x = event.clientX - dim.left;
             var y = event.clientY - dim.top;
             console.log("x: " + x + " y: " + y)
-            console.log("clientX: " + event.clientX + " clientY: " + event.clientY)
 
             if (isMouseDown === true) {
                 var circ = svg.circle(defaultSize)
-                shapes.push(circ.fill(svg.image(img1Path).loaded(function(loader) {
-                  this.size(loader.width * 0.25, loader.height * 0.25)
+                shapes.push(circ.fill(svg.image(botImgPath).loaded(function(loader) {
+                  this.size(loader.width * (1/3), loader.height * 0.32)
                 })))
-                circ.move(event.clientX - 50, event.clientY - 50)
-                console.log(shapes);
+                circ.move(x, y)
+/*                 console.log(shapes); */
             }
         }
 
         var mouseDown = function(event) {
           isMouseDown = true;
 
-          var e = event.target;
+/*        var e = event.target;
           var dim = e.getBoundingClientRect();
           var x = event.clientX - dim.left;
           var y = event.clientY - dim.top;
 
           var circ = svg.circle(defaultSize)
-                shapes.push(circ.fill(svg.image(img1Path).loaded(function(loader) {
-                  this.size(loader.width, loader.height)
+                shapes.push(circ.fill(svg.image(botImgPath).loaded(function(loader) {
+                  this.size(loader.width * (1/3), loader.height * 0.32)
                 })))
-                circ.move(x - 50, y - 50)
+                circ.move(x - 50, y - 50) */
         }
 
         var mouseUp = function() {
